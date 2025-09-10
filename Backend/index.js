8e5d764726c4c057f4ce6b394e5fd8af5800f0ba
unknown <ajaykarthikeyan436@gmail.com>
2025-09-10 16:24:10 +0530

    Updated Backend

--- Backend/index.js
+++ Backend/index.js
@@ -6,7 +6,15 @@ const employeeRoutes = require("./routes/employeeRoute");
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: [
+    'http://localhost:5173', 
+    'https://employee-management-system-fshr.vercel.app',
+    'https://tech-bazaar-frontend-d1vorsh3y-ajays-projects-8ce49744.vercel.app',//fallback Url for frontend
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true,
+}));
 app.use(express.json())
 
 //Connections
@@ -22,3 +30,5 @@ app.get("/", (req,res) => {
 app.listen(5000, () => {
     console.log("Server is Running....")
 })
+
+module.export = app;
